ed675bc9bd2edc58340eaf48cc8499673088857d
Jalen Lum <808jalen@gmail.com>
2025-01-28 13:11:40 -1000

    updated styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,17 @@
-import { Col, Container } from 'react-bootstrap';
-
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="bg-light mt-auto py-3">
-    <Container>
-      <Col className="text-center">
-        Team USA 2025
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        <br />
-        <a href="https://teamusa2025.github.io/">Project Home Page</a>
-      </Col>
-    </Container>
+  <footer className="m-4 rounded-lg bg-white dark:bg-gray-900">
+    <div className="mx-auto w-full max-w-screen-xl p-4 md:py-8">
+      <hr className="my-6 w-full border-t-2 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
+      <span className="block text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+        © 2025
+        {' '}
+        <a href="https://teamusa2025.github.io/" className="text-gray-500 hover:underline dark:text-gray-400">
+          TeamUSA2025™
+        </a>
+        . All Rights Reserved.
+      </span>
+    </div>
   </footer>
 );
 
