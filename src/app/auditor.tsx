e5b091b819fea9748087c253214a63aad3a6449c
JavinSol <javinsol3@gmail.com>
2025-01-28 15:40:00 -1000

    Create auditor.tsx

---
+++ src/app/auditor.tsx
@@ -0,0 +1,14 @@
+'use auditor';
+
+import { Container } from 'react-bootstrap';
+
+/** The Home page. */
+const Auditor = () => (
+  <main>
+    <Container id="landing-page" fluid className="py-3">
+      <h1>This is for the Audit Page</h1>
+    </Container>
+  </main>
+);
+
+export default Auditor;
