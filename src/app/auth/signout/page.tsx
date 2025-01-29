559cc3ed6e32d658476e335af544261cfbcddb11
Jalen Lum <808jalen@gmail.com>
2025-01-29 11:08:03 -1000

    Fixed button

--- src/app/auth/signout/page.tsx
+++ src/app/auth/signout/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { signOut } from 'next-auth/react';
+
 /** After the user clicks the "SignOut" link in the NavBar, display this page to ensure they want to sign out. */
 const SignOut = () => (
   <section className="my-20 bg-white py-2 dark:bg-gray-900">
@@ -8,14 +10,15 @@ const SignOut = () => (
         <p className="mb-4 py-5 text-3xl font-bold tracking-tight text-gray-900 dark:text-white md:text-3xl">
           Are you sure you want to sign out?
         </p>
-        <a
-          href="/"
+        <button
+          type="button"
+          onClick={() => signOut({ callbackUrl: '/', redirect: true })}
           className="focus:ring-primary-300 dark:focus:ring-primary-900 mx-20 my-5 inline-flex rounded-lg
           bg-red-600 px-5 py-2.5 text-center text-sm font-medium text-white no-underline hover:bg-red-800
           focus:outline-none focus:ring-4"
         >
           Sign Out
-        </a>
+        </button>
         <a
           href="/"
           className="focus:ring-primary-300 dark:focus:ring-primary-900 mx-20 my-5 inline-flex rounded-lg
