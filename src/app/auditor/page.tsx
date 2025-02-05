df95f9ab60bfc2c3cd7b7ac1eea5bb4c7f725578
JavinSol <javinsol3@gmail.com>
2025-02-04 15:59:56 -1000

    yes

--- src/app/auditor/page.tsx
+++ src/app/auditor/page.tsx
@@ -1,19 +1,23 @@
 'use client';
 
+import { Container } from 'react-bootstrap';
+
 /** The Auditor page. */
 const Auditor = () => (
   <main>
-    <h1 className="center">Mockup Page for Auditor Home Page</h1>
-    <div className="center">
-      <a href="/1">
-        <button
-          type="button"
-          className="mb-2 me-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800"
-        >
-          Edit
-        </button>
-      </a>
-    </div>
+    <Container id="landing-page" fluid className="mt-20 py-3">
+      <h1 className="center">Mockup Page for Auditor Home Page</h1>
+      <div className="center">
+        <a href="/1">
+          <button
+            type="button"
+            className="mb-2 me-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800"
+          >
+            Edit
+          </button>
+        </a>
+      </div>
+    </Container>
   </main>
 );
 
