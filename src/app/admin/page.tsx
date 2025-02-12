3cbb1d30bdcac471ee1c1f70efacf1ba674759a1
lippsd <156867150+lippsd@users.noreply.github.com>
2025-02-12 04:23:12 -1000

    Create Accounts Management page and implemented edit user form

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from 'next-auth';
 import { Col, Container, Row, Table } from 'react-bootstrap';
-import StuffItemAdmin from '@/components/StuffItemAdmin';
+import UserRowAdmin from '@/components/UserRowAdmin';
 import { prisma } from '@/lib/prisma';
 import { adminProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
@@ -12,30 +12,30 @@ const AdminPage = async () => {
       user: { email: string; id: string; randomKey: string };
     } | null,
   );
-  const stuff = await prisma.stuff.findMany({});
+  // const stuff = await prisma.stuff.findMany({});
   const users = await prisma.user.findMany({});
-
   return (
     <main>
       <Container id="list" fluid className="mt-20 py-3">
         <Row>
-          <Col>
+          {/* IMPLEMENT BELOW IN ANOTHER PAGE OR DELETE */}
+          {/* <Col>
             <h1>Admin</h1>
             <h4 className="mt-10">Income Statement (Audited Data)</h4>
             <Table striped bordered hover>
               <thead>
                 <tr>
-                  {/* <th>Year</th>
+                  <th>Year</th>
                   <th>Revenue</th>
-                  <th>Net Sales (calculate)</th> */}
-                  {/* Cost of Goods sold */}
-                  {/* <th>Cost of Contracting</th>
+                  <th>Net Sales (calculate)</th> 
+                  COMMENTED Cost of Goods sold
+                  <th>Cost of Contracting</th>
                   <th>Overhead</th>
                   <th>Cost of Goods sold (calculate)</th>
                   <th>Gross profit (calculate)</th>
-                  <th>Gross margin % (calculate)</th> */}
-                  {/* Operating expenses */}
-                </tr>
+                  <th>Gross margin % (calculate)</th>
+                  COMMENTED Operating expenses
+                  </tr>
               </thead>
               <tbody>
                 {stuff.map((item) => (
@@ -43,7 +43,7 @@ const AdminPage = async () => {
                 ))}
               </tbody>
             </Table>
-          </Col>
+          </Col> */}
         </Row>
         <Row>
           <Col>
@@ -54,14 +54,12 @@ const AdminPage = async () => {
                   <th>Username</th>
                   <th>Email</th>
                   <th>Role</th>
+                  <th>Subrole</th>
                 </tr>
               </thead>
               <tbody>
                 {users.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.email}</td>
-                    <td>{user.role}</td>
-                  </tr>
+                  <UserRowAdmin key={user.id} {...user} />
                 ))}
               </tbody>
             </Table>
---
+++ src/components/EditUserForm.tsx
@@ -0,0 +1,123 @@
+'use client';
+
+import { Button, Card, Col, Container, Form, Row } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import swal from 'sweetalert';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Role, Subrole, User } from '@prisma/client';
+import { EditUserSchema } from '@/lib/validationSchemas';
+import { editUser } from '@/lib/dbActions';
+
+const onSubmit = async (data: User) => {
+  // console.log(`onSubmit data: ${JSON.stringify(data, null, 2)}`);
+  await editUser(data);
+  swal('Success', 'Your item has been updated', 'success', {
+    timer: 2000,
+  });
+};
+
+const EditUserForm = ({ user }: { user: User }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<User>({
+    resolver: yupResolver(EditUserSchema),
+  });
+  // console.log(user);
+
+  return (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={5}>
+          <Col className="text-center">
+            <h2>Edit User</h2>
+          </Col>
+          <Card>
+            <Card.Body>
+              <Form onSubmit={handleSubmit(onSubmit)}>
+                <input type="hidden" {...register('id')} value={user.id} />
+                <Form.Group>
+                  <Form.Label>Username</Form.Label>
+                  <input
+                    type="text"
+                    {...register('username')}
+                    defaultValue={user.username}
+                    required
+                    className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+                  />
+                  <div className="invalid-feedback">{errors.username?.message}</div>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Email</Form.Label>
+                  <input
+                    type="text"
+                    {...register('email')}
+                    defaultValue={user.email}
+                    className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                  />
+                  <div className="invalid-feedback">{errors.email?.message}</div>
+                </Form.Group>
+                {/* PASSWORD FIELD HERE; SHOWS HASHEDPASSWORD ONLY AND EDITS ARE UNPREDICTABLE */}
+                <input type="hidden" {...register('password')} value={user.password} />
+                {/* <Form.Group>
+                  <Form.Label>Password</Form.Label>
+                  <input
+                    type="text"
+                    {...register('password')}
+                    defaultValue={user.password}
+                    className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                  />
+                  <div className="invalid-feedback">{errors.password?.message}</div>
+                </Form.Group> */}
+                <Form.Group>
+                  <Form.Label>Role</Form.Label>
+                  <select
+                    {...register('role')}
+                    className={`form-control ${errors.role ? 'is-invalid' : ''}`}
+                    defaultValue={user.role}
+                  >
+                    <option value={Role.USER}>User</option>
+                    <option value={Role.ADMIN}>Admin</option>
+                  </select>
+                  <div className="invalid-feedback">{errors.role?.message}</div>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Subrole</Form.Label>
+                  <select
+                    {...register('subrole')}
+                    className={`form-control ${errors.subrole ? 'is-invalid' : ''}`}
+                    defaultValue={user.subrole}
+                  >
+                    <option value={Subrole.ADMIN}>Admin</option>
+                    <option value={Subrole.EXECUTIVE}>Executive</option>
+                    <option value={Subrole.ANALYST}>Analyst</option>
+                    <option value={Subrole.AUDITOR}>Auditor</option>
+                  </select>
+                  <div className="invalid-feedback">{errors.subrole?.message}</div>
+                </Form.Group>
+                <Form.Group className="form-group">
+                  <Row className="pt-3">
+                    <Col>
+                      <Button type="submit" variant="primary">
+                        Submit
+                      </Button>
+                    </Col>
+                    <Col>
+                      <Button type="button" onClick={() => reset()} variant="warning" className="float-right">
+                        Reset
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form.Group>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default EditUserForm;
