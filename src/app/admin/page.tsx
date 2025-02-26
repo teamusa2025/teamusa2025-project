3123930973b73a44ab2516db2b4515399173e154
Ciara Kang <ckof2022@gmail.com>
2025-02-25 17:55:20 -1000

    update admin and executive permissions

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -22,34 +22,6 @@ const AdminPage = async () => {
     <main>
       <Container id="list" fluid className="mt-20 py-3">
         <Row>
-          {/* IMPLEMENT BELOW IN ANOTHER PAGE OR DELETE */}
-          {/* <Col>
-            <h1>Admin</h1>
-            <h4 className="mt-10">Income Statement (Audited Data)</h4>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Year</th>
-                  <th>Revenue</th>
-                  <th>Net Sales (calculate)</th>
-                  COMMENTED Cost of Goods sold
-                  <th>Cost of Contracting</th>
-                  <th>Overhead</th>
-                  <th>Cost of Goods sold (calculate)</th>
-                  <th>Gross profit (calculate)</th>
-                  <th>Gross margin % (calculate)</th>
-                  COMMENTED Operating expenses
-                  </tr>
-              </thead>
-              <tbody>
-                {stuff.map((item) => (
-                  <StuffItemAdmin key={item.id} {...item} />
-                ))}
-              </tbody>
-            </Table>
-          </Col> */}
-        </Row>
-        <Row>
           <Col>
             <h4>Accounts Management</h4>
             <Table striped bordered hover>
--- src/app/executive/page.tsx
+++ src/app/executive/page.tsx
@@ -1,14 +1,38 @@
 'use client';
 
 import { Container } from 'react-bootstrap';
+import { getServerSession } from 'next-auth';
+import { useEffect } from 'react';
+import { executiveProtectedPage } from '@/lib/page-protection';
+import authOptions from '@/lib/authOptions';
 
 /** The Executive page. */
-const Executive = () => (
-  <main>
-    <Container id="landing-page" fluid className="mt-20 py-3">
-      <h1>This is the Executive Page</h1>
-    </Container>
-  </main>
-);
+const Executive = () => {
+  useEffect(() => {
+    const fetchSession = async () => {
+      const session = await getServerSession(authOptions);
+      try {
+        executiveProtectedPage(
+          session as {
+            user: { email: string; id: string; randomKey: string };
+          } | null,
+        );
+        return session;
+      } catch (error) {
+        return <div>Access denied</div>;
+      }
+    };
+
+    fetchSession();
+  }, []);
+
+  return (
+    <main>
+      <Container id="landing-page" fluid className="mt-20 py-3">
+        <h1>This is the Executive Page</h1>
+      </Container>
+    </main>
+  );
+};
 
 export default Executive;
