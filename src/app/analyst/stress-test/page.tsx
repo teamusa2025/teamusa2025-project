dcf54e98f204be129564f426df5bbbc32659fd9f
Jalen Lum <808jalen@gmail.com>
2025-02-28 14:03:22 -1000

    login redirect and sidebar icons

--- src/app/analyst/stress-test/page.tsx
+++ src/app/analyst/stress-test/page.tsx
@@ -1,15 +1,13 @@
 'use client';
 
-import Analyst from '../page';
-
 /** The Analyst page. */
 const StressTest = () => (
-  <Analyst>
+  <>
     {/* eslint-disable-next-line tailwindcss/no-arbitrary-value */}
     <section className="ml-[185px] bg-white dark:bg-gray-900">
       <h2>This is the StressTest Page</h2>
     </section>
-  </Analyst>
+  </>
 );
 
 export default StressTest;
--- src/components/Siderbar.tsx
+++ src/components/Siderbar.tsx
@@ -155,7 +155,25 @@ const Sidebar: React.FC = () => {
                       className="group flex items-center rounded-md px-3 py-2 text-sm
                       font-medium text-gray-300 no-underline transition-colors hover:bg-gray-700"
                     >
-                      <span className="ml-6">
+                      <svg
+                        className="mr-2 size-6 text-gray-300 dark:text-white"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="21"
+                        height="21"
+                        fill="currentColor"
+                        viewBox="0 1 22 22"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M8 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1h2a2 2 0 0 1 2 2v15a2 2 0 0 1-2 2H6a2 2 0 0
+                          1-2-2V5a2 2 0 0 1 2-2h2Zm6 1h-4v2H9a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2h-1V4Zm-6 8a1 1
+                          0 0 1 1-1h6a1 1 0 1 1 0 2H9a1 1 0 0 1-1-1Zm1 3a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2H9Z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+
+                      <span>
                         Scenario
                         {' '}
                         {scenarioNumber}
